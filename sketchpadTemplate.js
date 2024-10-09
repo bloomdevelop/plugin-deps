cd7b9e526254d10f69337c1e612529c21577455c
Bloom <129090449+bloomdevelop@users.noreply.github.com>
2024-10-09 09:58:27 -0400

    feat: styled buttons

--- sketchpadTemplate.js
+++ sketchpadTemplate.js
@@ -64,6 +64,18 @@ window.sketchpad._css = `
   flex-direction: row;
   gap: .5rem;
 }
+
+.custom-button {
+  background-color: #0a0c1d;
+	border: none;
+	color: #fff;
+	cursor: pointer;
+	padding: 0.5rem;
+	text-align: center;
+	text-decoration: none;
+	transition: all 0.2s ease-in-out;
+	border-radius: 2rem;
+}
     `;
 
 window.sketchpad._html = `
@@ -73,23 +85,23 @@ window.sketchpad._html = `
 		</header>
 		<div class="tooling">
 			<div class="picker">
-				<button id="#ff0000">Red</button>
-				<button id="#0000ff">Blue</button>
-				<button id="#00ff00"">Green</button>
-				<button id="#FFFF00">Yellow</button>
-				<button id="#FFA500">Orange</button>
-				<button id="#800080">Purple</button>
-				<button id="#FFC0CB">Pink</button>
+				<button class="custom-button" id="#ff0000">Red</button>
+				<button class="custom-button" id="#0000ff">Blue</button>
+				<button class="custom-button" id="#00ff00"">Green</button>
+				<button class="custom-button" id="#FFFF00">Yellow</button>
+				<button class="custom-button" id="#FFA500">Orange</button>
+				<button class="custom-button" id="#800080">Purple</button>
+				<button class="custom-button" id="#FFC0CB">Pink</button>
 				<input type="color" value="#000" id="colour">
 			</div>
 			<div class="pentools">
-				<button id="pen">Pen</button>
-				<button id="eraser">Eraser</button>
-				<button id="10">10px</button>
-				<button id="20">20px</button>
-				<button id="45">45px</button>
-				<button id="65">65px</button>
-				<button id="100">100px</button>
+				<button class="custom-button" id="pen">Pen</button>
+				<button class="custom-button" id="eraser">Eraser</button>
+				<button class="custom-button" id="10">10px</button>
+				<button class="custom-button" id="20">20px</button>
+				<button class="custom-button" id="45">45px</button>
+				<button class="custom-button" id="65">65px</button>
+				<button class="custom-button" id="100">100px</button>
 				<input type="number" placeholder="Custom Size..." value=5 min=1 id="size">
 			</div>
 		</div>
